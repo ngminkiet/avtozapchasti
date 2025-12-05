49dd52d440aa66d49ad98a108ade305765af7b0a
ngminkiet <nguyen28071973@gmail.com>
2025-12-05 19:54:51 +0500

    добавил введение почты и пароля

--- avtozapchasti/avtozapchasti_app/urls.py
+++ avtozapchasti/avtozapchasti_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'), # name - это имя маршрута
+    path('auf', views.auf, name='auf')
 ]
