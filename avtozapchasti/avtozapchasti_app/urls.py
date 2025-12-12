e32bc2f383ecaa677f7d0ec9c0b567f95b331f1f
ngminkiet <nguyen28071973@gmail.com>
2025-12-12 19:52:50 +0500

    я добавил авторизацию

--- avtozapchasti/avtozapchasti_app/urls.py
+++ avtozapchasti/avtozapchasti_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # name - это имя маршрута
-    path('auf', views.auf, name='auf')
+    path('auf/', views.auf, name='auf')
 ]
--- avtozapchasti/avtozapchasti_app/views.py
+++ avtozapchasti/avtozapchasti_app/views.py
@@ -4,4 +4,10 @@ def index(request):
     return render(request, 'index.html')
 
 def auf(request):
-    return render(request, 'auf.html')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        print(email, password)
+    else:
+        return render(request, 'auf.html')
