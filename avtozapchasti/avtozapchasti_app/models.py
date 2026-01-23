03b5ba7f69d1a2094dc21787c4cbe7a3aba7e7f7
ngminkiet <nguyen28071973@gmail.com>
2026-01-23 19:50:55 +0500

    сделал модели

--- avtozapchasti/avtozapchasti_app/models.py
+++ avtozapchasti/avtozapchasti_app/models.py
@@ -1,3 +1,22 @@
+import os
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+#CharField - текстовое поле
+#IntegerField - целочисленное поле
+#FloatField - дробное поле
+#DateField - поле даты
+
+def images_path():
+    return os.path.join(settings.LOCAL_FILE_DIR, "images")
+
+def item_discription_path():
+    return os.path.join(settings.LOCAL_FILE_DIR, "item_discription")
+
+class Item(models.Model):
+    item_title = models.CharField(max_length=50) # заголовок товары
+    price = models.IntegerField() # цена
+    description = models.FilePathField(path=item_discription_path) #описание
+    material = models.CharField(max_length=20) # материал
+    spare_parts_type = models.CharField(max_length=20)# тип запчасти
+    spare_parts_color = models.CharField(max_length=20)# цвет запчасти
